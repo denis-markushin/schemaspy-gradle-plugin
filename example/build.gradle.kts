cacf5ef3be105c09df9899e934d1568a672a962d
Denis Markushin <denis.markushin@gmail.com>
2024-03-29 01:44:22 +0300

    chore(plugin-publication) Change domain name to folllow the gradle plugin portal rules
    * test fix

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -6,5 +6,4 @@ plugins {
 schemaspyConfig {
     dbName = "tickets"
     liquibaseChangelog = file("${project.projectDir}/src/main/resources/liquibase/changelog-master.yml")
-    postgresDockerImage = "docker.samokat.io/dockerhub/postgres:12.8-alpine"
 }

fe1bef93bb32d1f5f22301035624713df97a1642
Denis Markushin <denis.markushin@gmail.com>
2024-10-10 20:19:42 +0300

    refactor: small refactoring

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -1,10 +1,9 @@
 plugins {
-    java
     id("io.github.denis-markushin.schemaspy-plugin")
 }
 
 schemaspyConfig {
     dbName = "sakila"
     liquibaseChangelog = file("${project.projectDir}/src/main/resources/liquibase/changelog.yml")
-    outputDir = project.layout.buildDirectory.dir("schemaspy/sakila").get()
+    outputDir = project.layout.buildDirectory.dir("schemaspy/sakila")
 }
